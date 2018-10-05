71183244e108013d50a558cb1e817c2eaed9aeeb
Kevin Matthews <kmatthews@cyberdata.net>
2018-10-04 19:41:32 -0700

    Expand server to server a page and handle socketio

--- werkzeug/README.md
+++ werkzeug/README.md
@@ -1,15 +1,19 @@
 # Flask-SocketIO with Werkzeug
 
-Only viable for debug mode.
-
+Run flask served by Werkzeug's dev server:
 ```
 $ FLASK_ENV=development
 # flask auto-detects app.py
 $ flask run
 ```
-or
+Only viable for debug mode - in production mode it fails and I don't know why.
+Wrapping the flask app in the SocketIO app seems to cause this issue.
+
+Alternatively, run manually:
 ```
 $ python index.py
 ```
+"production" or debug.
+
 
 Will fail if in production environment because eventlet/gevent aren't installed in the venv.
--- werkzeug/app.py
+++ werkzeug/app.py
@@ -1,11 +1,42 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
+SERVER_HOSTNAME = ''
+SERVER_PORT = 5000
+
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='threading')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def connect():
+    print('connect')
+
+@socketio.on('disconnect')
+def disconnect():
+    print('disconnect')
+
+# 'message' is the default event for a SocketIO client's send() call.
+@socketio.on('message')
+def message(data):
+    print('message:', data)
+
+# Custom event
+@socketio.on('echo')
+def echo(data):
+    print('echo:', data)
+    socketio.emit('echo', data)
+
+@socketio.on('client_callback')
+def client_callback(data):
+    print('client_callback:', data)
+    return 'Server Response 1', 'Server Response 2'
 
 if __name__ == '__main__':
     # Replaces call to flask's app.run()
     # Runs with Werkzeug if FLASK_ENV=deployment
     # Runs with eventlet or gevent otherwise
-    socketio.run(app)
+    socketio.run(app, host=SERVER_HOSTNAME, port=SERVER_PORT)
